1fc7bb01c5317bd3adef01bc3a2d5cb2609c60b8
grantpant <grantpant@gmail.com>
2019-05-26 21:31:00 -0500

    Add User type to schema; set up createUser resolver

--- src/resolvers/Mutation.js
+++ src/resolvers/Mutation.js
@@ -1,4 +1,28 @@
+import bcrypt from 'bcryptjs';
+import jwt from 'jsonwebtoken';
+
 const Mutation = {
+  async createUser(parent, args, { prisma }, info) {
+    const createUserInput = {
+      data: {
+        ...args.data,
+        password: await bcrypt.hash(args.data.password, 10)
+      }
+    };
+
+    const user = await prisma.mutation.createUser(createUserInput);
+
+    const token = jwt.sign(
+      { userId: user.id },
+      process.env.JWT_SECRET,
+      { expiresIn: '7 days' }
+    );
+
+    return {
+      user,
+      token
+    };
+  },
   async createExercise(parent, args, { prisma }, info) {
     const { name, bodySection, primaryMover, movementType, trainingPhases, workoutTypes, equipment } = args.data;
 
